33c20feca7bcf7a865944487d040222e022f4ef6
merdolda-source <merdolda@gmail.com>
2026-06-19 21:48:02 +0300

    Add app/src/main/java/com/ere/derws/SplashActivity.kt

---
+++ app/src/main/java/com/ere/derws/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.ere.derws
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        splashScreen.setKeepOnScreenCondition { false }
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
